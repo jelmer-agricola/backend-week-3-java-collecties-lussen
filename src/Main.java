ad33cd12fd851de386577b90877bb7cf78ec502c
jelmer-agricola <jelmer.agricola@gmail.com>
2022-09-21 08:54:32 +0200

    huiswerk

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeric = new ArrayList<>(Arrays.asList(1, 2, 4, 5, 6, 7, 8,9,0));
+        ArrayList<String> alphabetic = new ArrayList<>(Arrays.asList("one", "two", "three" ,"four", "five" , "six" ,"seven", "eigth", "nine", "zero"));
+
+    }
+}
